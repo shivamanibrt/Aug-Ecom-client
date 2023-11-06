37ac4431a82d854763c0dd50ca4cbe37725c5892
shivamanibrt <shivamanibrt03350@gmail.com>
2023-11-07 06:48:52 +1100

    added produce

--- src/Pages/Products/NewProduct.js
+++ src/Pages/Products/NewProduct.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { AdminLayout } from '../../Layout/AdminLayout'
-import { Button, Container } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import { Form } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 import { BiArrowBack } from 'react-icons/bi'
+import { CustomInput } from '../../Component/FormComponent/CustomInput'
 
 
 export const NewProduct = () => {
@@ -12,32 +13,84 @@ export const NewProduct = () => {
     const handelOnClick = () => {
         navigate('/products')
     }
-    const itemField = {
-        name: {
-
+    const inputField = [
+        {
+            name: 'name',
+            label: 'Name',
+            type: 'text',
+            placeholder: 'Product Name',
+            required: true
+        },
+        {
+            name: 'sku',
+            label: 'SKU',
+            type: 'text',
+            placeholder: 'Product Unique Code',
+            required: true
+        },
+        {
+            name: 'price',
+            label: 'Price',
+            type: 'number',
+            placeholder: '343',
+        },
+        {
+            name: 'qty',
+            label: 'Qty',
+            type: 'number',
+            placeholder: '50',
+        },
+        {
+            name: 'salesStartDate',
+            label: 'Sale Start Date',
+            type: 'date',
+            placeholder: '100',
+        },
+        {
+            name: 'SalesEndDate',
+            label: 'sales End Date',
+            type: 'date',
+            placeholder: '100',
+        },
+        {
+            name: 'description',
+            label: 'Description',
+            type: 'text',
+            as: 'textarea',
+            rows: '5',
+            placeholder: 'Description',
+            required: true
+        },
+        {
+            name: 'images',
+            type: 'file',
+            accept: 'images'
         }
-    }
-    // "name": "Sqckbook",
-    //     "sku": "macs_14",
-    //         "description": "Mac intel chip",
-    //             "qty": 100,
-    //                 "price": 2000,
-    //                     "salesPrice": 0,
-    //                         "salesStartDate": null,
-    //                             "salesEndDate": null,
-    //                                 "catId": "653a30a2e0dc67a70fe3dc28"
+    ]
 
     return (
         <AdminLayout>
+            <div className="p-2 align-items-center">
+                <div onClick={handelOnClick}><BiArrowBack />Back</div>
+                <h1>Add New Product</h1>
+            </div>
+            <hr />
             <Container>
-                <div className='text-start mt-2'>
-                    <Button variant='primary' className="d-flex align-items-center" onClick={handelOnClick}><BiArrowBack />Go Back</Button>
-                </div>
-                <Form>
-                    Hello
+
+                <Form className='p-2 shadow-lg product-card' >
+                    <Form.Group className='mb-2'>
+                        <Form.Check name='status' type='switch' label='status' />
+
+                    </Form.Group>
+
+                    <Form.Group>
+
+                    </Form.Group>
+                    {inputField.map((item, i) =>
+                        <CustomInput {...item} />)}
                 </Form>
             </Container>
-        </AdminLayout>
+        </AdminLayout >
 
     )
 }
